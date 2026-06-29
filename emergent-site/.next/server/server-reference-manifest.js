8c8dc06b4bdc80f30b3d29c745d614b6eb0d5a3d
khushnawriya30-del <khushnawriya30-del@users.noreply.github.com>
2026-06-29 13:50:36 +0530

    chore: stop tracking standalone 3D-site folders in main repo

--- emergent-site/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DxYCbM5/L3TyHaRdjngeCn9vYqZSP1JYjpDDvq0QZ0g=\"\n}"
--- emergent-site/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // R3F dislikes StrictMode's double-invoke during dev.
-  reactStrictMode: false,
-};
-
-module.exports = nextConfig;
